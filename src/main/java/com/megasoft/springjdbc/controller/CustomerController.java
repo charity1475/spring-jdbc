4ffab794351db42f96e3bfeffea9bc62c8f3d5e9
Charity <charitymbisi@gmail.com>
2024-03-03 01:23:16 +0300

    feat: controller and repo

--- src/main/java/com/megasoft/springjdbc/controller/CustomerController.java
+++ src/main/java/com/megasoft/springjdbc/controller/CustomerController.java
@@ -1,2 +1,35 @@
-package com.megasoft.springjdbc.controller;public class CustomerController {
+package com.megasoft.springjdbc.controller;
+
+import com.megasoft.springjdbc.models.Customer;
+import com.megasoft.springjdbc.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/1.0.0/customers")
+public class CustomerController {
+
+	private final CustomerRepository customerRepository;
+
+	@Autowired
+	public CustomerController(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	@GetMapping("")
+	public ResponseEntity<Iterable<Customer>> getAllCustomers(Pageable pageable) {
+		Page<Customer> page = customerRepository.findAll(PageRequest.of(pageable.getPageNumber(),
+				pageable.getPageSize(), pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))));
+
+		return ResponseEntity.ok(page.getContent());
+	}
+
 }
--- src/main/java/com/megasoft/springjdbc/repository/CustomerRepository.java
+++ src/main/java/com/megasoft/springjdbc/repository/CustomerRepository.java
@@ -1,2 +1,11 @@
-package com.megasoft.springjdbc.repository;public interface CustomerRepository {
+package com.megasoft.springjdbc.repository;
+
+import com.megasoft.springjdbc.models.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long>, PagingAndSortingRepository<Customer, Long> {
+
 }
