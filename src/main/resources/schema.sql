828ecf6bce2d8f9076ff17cbdf655232353b4247
Charity <charitymbisi@gmail.com>
2024-03-02 16:28:34 +0300

    feat: docker composer support

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+create table if not exists customer
+(
+  id serial primary key,
+  name text
+);
+
+create table if not exists customer_orders
+(
+  id serial primary key,
+  customer bigint not null references customer(id),
+  name varchar(200)
+);
+
+create table if not exists customer_profile
+(
+  id serial primary key,
+  customer bigint not null references customer(id),
+  username text not null unique,
+  password text not null
+);
+
+delete from customer_profile;
+delete from customer_orders;
+delete from customer;
+
